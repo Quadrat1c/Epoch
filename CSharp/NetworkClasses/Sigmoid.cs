07c6b7dfa496dc79590f40d0b250a692f8593d3e
quadrat1c <w4rd3nbot@gmail.com>
2017-04-28 11:49:28 -0700

    Create Sigmoid.cs

---
+++ CSharp/NetworkClasses/Sigmoid.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Epoch.Network
+{
+    public static class Sigmoid
+    {
+        public static double Output(double x)
+        {
+            return x < -45.0 ? 0.0 : x > 45.0 ? 1.0 : 1.0 / (1.0 + Math.Exp(-x));
+        }
+
+        public static double Derivative(double x)
+        {
+            return x * (1 - x);
+        }
+    }
+}
