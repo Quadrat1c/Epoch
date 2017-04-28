9e1b06db1df77f17c151042cea62ac79ba24fd36
quadrat1c <w4rd3nbot@gmail.com>
2017-04-28 11:48:36 -0700

    Create Dataset.cs

---
+++ CSharp/NetworkClasses/Dataset.cs
@@ -0,0 +1,18 @@
+namespace Epoch.Network
+{
+    public class DataSet
+    {
+        #region -- Properties --
+        public double[] Values { get; set; }
+        public double[] Targets { get; set; }
+        #endregion
+
+        #region -- Constructor --
+        public DataSet(double[] values, double[] targets)
+        {
+            Values = values;
+            Targets = targets;
+        }
+        #endregion
+    }
+}
