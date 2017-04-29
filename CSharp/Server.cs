34d25bacca5e5578981fd51dca642533c5f30827
quadrat1c <w4rd3nbot@gmail.com>
2017-04-29 12:14:02 -0700

    Update Server.cs

--- CSharp/Server.cs
+++ CSharp/Server.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 
 namespace Epoch
@@ -23,7 +25,10 @@ namespace Epoch
         public void Run()
         {
             writer.WriteLine(cmd.intro());
+
             // get clients ip address
+            string clientIp = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+            Console.WriteLine("New Connection: " + clientIp);
 
             // command loop - swtch
             for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
@@ -33,22 +38,27 @@ namespace Epoch
                     // [Main]
                     case "test":
                         writer.WriteLine("[CB} Test");
+                        Console.WriteLine("[test:" + clientIp + "}");
                         break;
 
                     case "help":
                         writer.WriteLine(cmd.help());
+                        Console.WriteLine("[help:" + clientIp + "}");
                         break;
 
                     case "about":
                         writer.WriteLine(cmd.about());
+                        Console.WriteLine("[about:" + clientIp + "}");
                         break;
 
                     case "ver":
                         writer.WriteLine(cmd.version());
+                        Console.WriteLine("[ver:" + clientIp + "}");
                         break;
 
                     default:
                         writer.WriteLine("Unknown Command: " + line);
+                        Console.WriteLine("[UC:" + clientIp + "} " + line);
                         break;
                 }
             }
