091481d312e3928619fc22aa339b098a1054611b
quadrat1c <w4rd3nbot@gmail.com>
2017-05-01 08:14:47 -0700

    Update GPIO.cs

--- CSharp/GPIO.cs
+++ CSharp/GPIO.cs
@@ -1,23 +1,7 @@
-using Raspberry.IO.GeneralPurpose;
-
 namespace Epoch
 {
     public class GPIO
     {
-        public static void blinkLed()
-        {
-            var led1 = ConnectorPin.P1Pin22.Output();
-
-            var connection = new GpioConnection(led1);
-
-            for (var i = 0; i < 100; i++)
-            {
-                // toggle
-                connection.Toggle(led1);
-                System.Threading.Thread.Sleep(250);
-            }
-
-            connection.Close();
-        }
+        // Need a working GPIO lib OR we will need to use bash uhg
     }
 }
